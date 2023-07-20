120f6fd5cb1859dbcf3650947e496bea0a119bc8
turtleE-code <H.turtleE@gmail.com>
2023-07-20 21:24:16 +0900

    update checkedButtun

--- checkedButtun/js/script.js
+++ checkedButtun/js/script.js
@@ -4,12 +4,14 @@ const btn = document.querySelector('#btn');
 isAgreed.addEventListener('change', () => {
     console.log(isAgreed.checked);
 
-    if(isAgreed.checked)
-    {
-        btn.disabled = false;
-    }
-    else
-    {
-        btn.disabled = true;
-    }
+    btn.disabled = !isAgreed.checked;
+
+    // if(isAgreed.checked)
+    // {
+    //     btn.disabled = false;
+    // }
+    // else
+    // {
+    //     btn.disabled = true;
+    // }
 });
